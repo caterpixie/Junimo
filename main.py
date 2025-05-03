752e22d98b2c362660966f9562f9713e36c0fd05
caterpixie <pixie7428@gmail.com>
2025-05-03 17:14:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ load_dotenv()
 import discord
 from discord.ext import commands
 import asyncpg
-#from setup import setup_chores, set_bot as setup_set_bot, delete_chores_table
 from qotd import qotd_group, auto_post_qotd, set_bot as set_qotd_bot
-from chores import set_bot as set_chores_bot
+from chores import set_bot as set_chores_bot, auto_post_chores
 
 class Client(commands.Bot):
     def __init__(self, **kwargs):
@@ -23,18 +22,16 @@ class Client(commands.Bot):
         print(f'Logged on as {self.user}')
         if not auto_post_qotd.is_running():
             auto_post_qotd.start()
+        
+        if not auto_post_chores.is_running():
+            auto_post_chores.start()
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = Client(command_prefix="!", intents=intents)
 
 # Set bot instance in each module
-#setup_set_bot(bot)
 set_qotd_bot(bot)
 set_chores_bot(bot)
-
-# Add setup commands
-#bot.tree.add_command(setup_chores)
-#bot.tree.add_command(delete_chores_table)
 
 bot.run(os.getenv("DISCORD_TOKEN"))
