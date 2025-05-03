0d127409d116c07389a0dcfddcf07e9553644083
caterpixie <pixie7428@gmail.com>
2025-05-02 20:47:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 import asyncpg
 from setup import setup_chores, set_bot as setup_set_bot, delete_chores_table
 from qotd import qotd_group, auto_post_qotd, set_bot as set_qotd_bot
-from chores import set_bot as set_chores_bot, chore_add
+from chores import set_bot as set_chores_bot
 
 class Client(commands.Bot):
     def __init__(self, **kwargs):
@@ -36,7 +36,5 @@ set_chores_bot(bot)
 # Add setup commands
 bot.tree.add_command(setup_chores)
 bot.tree.add_command(delete_chores_table)
-bot.tree.add_command(chore_add)
-
 
 bot.run(os.getenv("DISCORD_TOKEN"))
