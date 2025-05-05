60f9b11e981cb48cf2e88e998fbb6953b3879d1d
caterpixie <pixie7428@gmail.com>
2025-05-04 20:24:49 -0400

    Create uwu.py

---
+++ uwu.py
@@ -0,0 +1,15 @@
+import discord
+from discord import app_commands
+import re
+
+bot = None
+def set_bot(bot_instance):
+    global bot
+    bot = bot_instance
+
+@app_commands.command(name="uwu", description="UuW-ifies a message")
+async def uwu(interaction: discord.Interaction, message: str):
+    message = message.replace("r", "w").replace("R", "W")
+    message = message.replace("l", "w").replace("L", "W")
+
+    await interaction.response.send_message(message)
