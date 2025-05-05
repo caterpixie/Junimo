44f69dd8658862d4b37736739d886eb2caf6a8b9
caterpixie <pixie7428@gmail.com>
2025-05-04 21:14:42 -0400

    Update uwu.py

--- uwu.py
+++ uwu.py
@@ -1,6 +1,7 @@
 import discord
 from discord import app_commands
 import re
+import random
 
 bot = None
 def set_bot(bot_instance):
@@ -9,7 +10,9 @@ def set_bot(bot_instance):
 
 @app_commands.command(name="uwu", description="UuW-ifies a message")
 async def uwu(interaction: discord.Interaction, message: str):
-    message = message.replace("r", "w").replace("R", "W")
-    message = message.replace("l", "w").replace("L", "W")
-
-    await interaction.response.send_message("-#", message)
+    message = message.replace("r", "w").replace("l", "w")
+    message = message.replace("R", "W").replace("L", "W")
+    message = re.sub(r"n(?!\b)", "ny", message)
+    message = re.sub(r"N(?!\b)", "Ny", message)
+    
+    await interaction.response.send_message(f"> {message}")
