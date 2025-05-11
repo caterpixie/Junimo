b06debbf608b510b506dd274a1f4d06fb9df0bef
Pixie <pixie7428@gmail.com>
2025-05-10 21:28:55 -0400

    Update main.py

--- Junimo/main.py
+++ Junimo/main.py
@@ -6,9 +6,11 @@ import discord
 from discord.ext import commands
 import aiomysql
 import urllib.parse
+
 from qotd import qotd_group, auto_post_qotd, set_bot as set_qotd_bot
 from chores import set_bot as set_chores_bot, auto_post_chores
 from uwu import set_bot as set_uwu_bot, uwu
+from triggers import set_bot as set_trigger_bot
 
 class Client(commands.Bot):
     def __init__(self, **kwargs):
@@ -27,7 +29,6 @@ class Client(commands.Bot):
             db=parsed.path[1:],
             autocommit=True,
         )
-
         self.tree.add_command(qotd_group)
         self.tree.add_command(uwu)
         await self.tree.sync()
@@ -48,5 +49,6 @@ bot = Client(command_prefix="!", intents=intents)
 set_qotd_bot(bot)
 set_chores_bot(bot)
 set_uwu_bot(bot)
+set_trigger_bot(bot)
 
 bot.run(os.getenv("DISCORD_TOKEN"))
