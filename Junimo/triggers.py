48e96fb552c2057fc03005b121fdffe2717401d5
Pixie <pixie7428@gmail.com>
2025-05-10 21:30:53 -0400

    Update triggers.py

--- Junimo/triggers.py
+++ Junimo/triggers.py
@@ -1,21 +1,23 @@
 import discord
-from discord import app_commands
 
 bot = None
 def set_bot(bot_instance):
     global bot
     bot = bot_instance
 
-@bot.event
-async def on_message(message):
-    if message.author.bot:
-        return
+    @bot.event
+    async def on_message(message):
+        if message.author.bot:
+            return
 
-    triggers = await bot.pool.fetch("SELECT trigger_text, response_text FROM triggers WHERE guild_id = $1", message.guild.id)
+        triggers = await bot.pool.fetch(
+            "SELECT trigger_text, response_text FROM triggers WHERE guild_id = $1",
+            message.guild.id
+        )
 
-    for trigger in triggers:
-        if trigger["trigger_text"] in message.content.lower():
-            await message.channel.send(trigger["response_text"])
-            break 
+        for trigger in triggers:
+            if trigger["trigger_text"] in message.content.lower():
+                await message.channel.send(trigger["response_text"])
+                break
 
-    await bot.process_commands(message)
+        await bot.process_commands(message)
