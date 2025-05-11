863c7c768d7fc8ba3a09fe55ab41dc2d82d23c17
Pixie <pixie7428@gmail.com>
2025-05-10 22:08:57 -0400

    Update triggers.py

--- Junimo/triggers.py
+++ Junimo/triggers.py
@@ -1,27 +1,30 @@
 import discord
-import aiomysql
+import aiosql
 
 bot = None
 def set_bot(bot_instance):
     global bot
     bot = bot_instance
 
-    @bot.event
-    async def on_message(message):
-        if message.author.bot:
-            return
-    
-        async with bot.pool.acquire() as conn:
-            async with conn.cursor(aiomysql.DictCursor) as cur:
-                await cur.execute(
-                    "SELECT trigger_text, response_text FROM triggers WHERE guild_id = %s",
-                    (message.guild.id,)
-                )
-                triggers = await cur.fetchall()
-    
-        for trigger in triggers:
-            if trigger["trigger_text"] in message.content.lower():
-                await message.channel.send(trigger["response_text"])
-                break
-    
-        await bot.process_commands(message)
+@bot.event
+async def on_message(message: discord.Message):
+    if message.author.bot or not message.guild:
+        return
+
+    async with bot.pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute("""
+                SELECT trigger_text, response_type, response_text FROM triggers
+                WHERE guild_id = %s
+            """, (message.guild.id,))
+            rows = await cur.fetchall()
+
+    content = message.content.lower()
+    for trigger_text, response_type, response_text in rows:
+        if trigger_text.lower() in content:
+            if response_type == 'plain':
+                await message.channel.send(response_text)
+            elif response_type == 'embed':
+                embed = discord.Embed(description=response_text, color=discord.Color.purple())
+                await message.channel.send(embed=embed)
+            break
