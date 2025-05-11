2f7a693da9de98bb08f11bc08a9358c9161558db
Pixie <pixie7428@gmail.com>
2025-05-10 21:34:08 -0400

    Update triggers.py

--- Junimo/triggers.py
+++ Junimo/triggers.py
@@ -9,15 +9,18 @@ def set_bot(bot_instance):
     async def on_message(message):
         if message.author.bot:
             return
-
-        triggers = await bot.pool.fetch(
-            "SELECT trigger_text, response_text FROM triggers WHERE guild_id = $1",
-            message.guild.id
-        )
-
+    
+        async with bot.pool.acquire() as conn:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(
+                    "SELECT trigger_text, response_text FROM triggers WHERE guild_id = %s",
+                    (message.guild.id,)
+                )
+                triggers = await cur.fetchall()
+    
         for trigger in triggers:
             if trigger["trigger_text"] in message.content.lower():
                 await message.channel.send(trigger["response_text"])
                 break
-
+    
         await bot.process_commands(message)
