24707de3c6b2adc5af1e40554300416fbe878330
Pixie <pixie7428@gmail.com>
2025-05-31 14:26:47 -0400

    Update confessions.py

--- Junipriest/confessions.py
+++ Junipriest/confessions.py
@@ -141,18 +141,15 @@ class ApprovalView(View):
 
         new_message = await channel.send(embed=embed, view=ConfessionInteractionView(bot))
 
-        # Remove only the "Submit a Confession" button from the old message
+        # Remove old buttons
         last_message_id = get_latest_confession_id()
         if last_message_id:
             try:
                 old_message = await channel.fetch_message(last_message_id)
-                old_view = ConfessionInteractionView(bot)
-                for item in old_view.children:
-                    if item.custom_id == "confession_submit":
-                        old_view.remove_item(item)
-                await old_message.edit(view=old_view)
+                await old_message.edit(view=None)
             except discord.NotFound:
                 pass
+                
         set_latest_confession_id(new_message.id)
 
         await interaction.response.send_message("Approved and posted!", ephemeral=True)
