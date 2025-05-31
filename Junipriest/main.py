78e6802d0a6d2eabbbb8d56b55f693576acc8a7a
Pixie <pixie7428@gmail.com>
2025-05-31 14:58:46 -0400

    Update main.py

--- Junipriest/main.py
+++ Junipriest/main.py
@@ -7,7 +7,12 @@ from discord.ext import commands
 import aiomysql
 import urllib.parse
 
-from confessions import confession_group, reply_to_confession_context, set_bot as set_confessions_bot, ConfessionInteractionView, ApprovalView
+from confessions import (
+    confession_group,
+    reply_to_confession_context,
+    set_bot as set_confessions_bot,
+    ConfessionInteractionView
+)
 
 class Client(commands.Bot):
     def __init__(self, **kwargs):
@@ -16,8 +21,7 @@ class Client(commands.Bot):
 
     async def setup_hook(self):
         set_confessions_bot(self)
-        self.add_view(ConfessionInteractionView(self))
-        self.add_view(ApprovalView())
+        self.add_view(ConfessionInteractionView(self))  # ApprovalView removed
 
         db_url = os.getenv("DATABASE_URL")
         parsed = urllib.parse.urlparse(db_url)
@@ -29,10 +33,11 @@ class Client(commands.Bot):
             db=parsed.path[1:],
             autocommit=True,
         )
-        self.tree.add_command(confession_group, guild=discord.Object(id=1322072874214756375))
-        self.tree.add_command(reply_to_confession_context, guild=discord.Object(id=1322072874214756375))
-        
-        await self.tree.sync(guild=discord.Object(id=1322072874214756375))
+
+        guild_id = discord.Object(id=1322072874214756375)
+        self.tree.add_command(confession_group, guild=guild_id)
+        self.tree.add_command(reply_to_confession_context, guild=guild_id)
+        await self.tree.sync(guild=guild_id)
 
     async def on_ready(self):
         print(f'Logged on as {self.user}')
@@ -42,5 +47,4 @@ intents.message_content = True
 bot = Client(command_prefix="?", intents=intents)
 
 set_confessions_bot(bot)
-
 bot.run(os.getenv("DISCORD_TOKEN"))
