11496fccc4552f777e9321e7cba3c13cb6c3a6da
Pixie <pixie7428@gmail.com>
2025-05-31 17:22:39 -0400

    Update main.py

--- Junipriest/main.py
+++ Junipriest/main.py
@@ -8,7 +8,7 @@ import json
 
 from confessions import confession_group, reply_to_confession_context, set_bot as set_confessions_bot, ConfessionInteractionView, ApprovalView
 
-CONFESSION_APPROVAL_CHANNEL = 1378213253146218557
+CONFESSION_APPROVAL_CHANNEL=1322431042501738550
 
 async def restore_pending_confessions(bot):
     import json
